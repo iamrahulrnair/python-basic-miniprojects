18d6fd92a04f2937fdf3f5b5f628e10e00c8b7ec
Tolokov <vitaliitolokov@gmail.com>
2021-12-12 22:19:34 +0500

    import tkinter

--- red_and_blue_BUTTONS.py
+++ red_and_blue_BUTTONS.py
@@ -1,9 +1,15 @@
-import tkinter
+import tkinter as tk
 
-def main():
+
+def main(title):
     '''
     target: 3 label (red: +1 /counter/blue: stop counter)
     '''
+    window = tk.Tk()
+    window.geometry(f'250x100+800+150')
+    window.title(title)
+    window.mainloop()
+
 
 if __name__ == '__main__':
-    main()
+    main('red_and_blue')
