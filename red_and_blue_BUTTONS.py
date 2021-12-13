800b8a4b7a9edd83a90b93b7e48b4d134f225f71
Tolokov <vitaliitolokov@gmail.com>
2021-12-13 16:32:36 +0500

    create Progressbar

--- red_and_blue_BUTTONS.py
+++ red_and_blue_BUTTONS.py
@@ -1,5 +1,5 @@
 import tkinter as tk
-
+from tkinter import ttk
 
 def main(title):
     '''
@@ -8,7 +8,7 @@ def main(title):
 
     def if_finish():
         print(count, end=',')
-        if count == 100:
+        if count % 100 == 0:
             import tkinter.messagebox as mb
             title = "You WIN!"
             massage = "Congratulations!"
@@ -16,22 +16,30 @@ def main(title):
             window.destroy()
 
     def add_one():
-        '''red button add 3'''
-        global count
-        count = count + 3
+        '''red button global_count add 3'''
+        update_global_value(3)
         try:
-            And['text'] = f'{count}'
             if_finish()
         except BaseException as ex:
             print(f'Window is closed: {ex}')
 
     def stop_add():
         '''red button is freeze and global_count add -7'''
-        global count
-        count = count + (-7)
+        update_global_value(-7)
         status = 'disabled' if Red['state'] == 'normal' else 'normal'
         Red['state'] = status
 
+    def update_global_value(value):
+        global count
+        count = count + (value)
+        update_progress_bar()
+        And['text'] = f'{count}'
+
+    def update_progress_bar():
+        if count > 100 or count < -100:
+            bar['value'] = abs(count) - 100
+        else:
+            bar['value'] = abs(count)
 
     window = tk.Tk()
 
@@ -40,13 +48,15 @@ def main(title):
     window.title(title)
     window.grid()
 
-    Red = tk.Button(window, text='RED', bg='red', height=6, width=11, command=add_one)
-    And = tk.Label(window, text=f'{count}', bg='white', height=6, width=10)
-    Blue = tk.Button(window, text='BLUE', bg='#1E90FF', height=6, width=11, command=stop_add)
+    Red = tk.Button(window, text='RED', bg='red', height=5, width=11, command=add_one)
+    And = tk.Label(window, text=f'{count}', bg='white', height=5, width=10)
+    Blue = tk.Button(window, text='BLUE', bg='#1E90FF', height=5, width=11, command=stop_add)
+    bar = ttk.Progressbar(window, orient="horizontal", mode="determinate", maximum=100)
 
-    Red.grid(row=0, column=0, sticky='NSEW')
+    Red.grid(row=0, column=1, sticky='NSEW')
     Blue.grid(row=0, column=3)
     And.grid(row=0, column=2)
+    bar.grid(row=1, column=0, columnspan=4, sticky='NEW')
 
     return window
 
