21b21047517b2de6269b5cd6dd87c6f5fb24e748
Tolokov <vitaliitolokov@gmail.com>
2021-12-13 14:52:04 +0500

    Create message box 'if finish' and Blue button added command: count -= 2

--- red_and_blue_BUTTONS.py
+++ red_and_blue_BUTTONS.py
@@ -1,19 +1,35 @@
 import tkinter as tk
 
+
 def main(title):
     '''
     target: 3 label (red: +1 /counter/blue: stop counter)
     '''
 
+    def if_finish():
+        print(count)
+        if count == 100:
+            import tkinter.messagebox as mb
+            title = "You WIN!"
+            massage = "Congratulations!"
+            mb.showwarning(title, massage)
+            window.destroy()
+
     def add_one():
         global count
         count = count + 1
-        print(count)
-        And['text'] = f'{count}'
+        if_finish()
+        try:
+            And['text'] = f'{count}'
+        except BaseException as ex:
+            print(f'Window is closed: {ex}')
 
     def stop_add():
+        global count
+        count = count - 2
         status = 'disabled' if Red['state'] == 'normal' else 'normal'
         Red['state'] = status
+
 
     window = tk.Tk()
 
@@ -22,9 +38,9 @@ def main(title):
     window.title(title)
     window.grid()
 
-    Red = tk.Button(window, text='RED', bg='red', height = 6, width = 11, command=add_one)
-    And = tk.Label(window, text=f'{count}', bg='white', height = 6, width = 10)
-    Blue = tk.Button(window, text='BLUE', bg='#1E90FF',  height = 6, width = 11, command=stop_add)
+    Red = tk.Button(window, text='RED', bg='red', height=6, width=11, command=add_one)
+    And = tk.Label(window, text=f'{count}', bg='white', height=6, width=10)
+    Blue = tk.Button(window, text='BLUE', bg='#1E90FF', height=6, width=11, command=stop_add)
 
     Red.grid(row=0, column=0, sticky='NSEW')
     Blue.grid(row=0, column=3)
